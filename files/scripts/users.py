a2afbadf11c47969c727fc075f84b80d74501fc2
Mister-Stalker <pna24052006@yandex.ru>
2022-01-31 18:03:36 +0300

    a0.2 (31.01.2022)

--- files/scripts/users.py
+++ files/scripts/users.py
@@ -1,22 +1,31 @@
 import json
 import os
 import pymongo
+from bson.objectid import ObjectId
 
 
 class User:
-    def __init__(self, user_id: int):
-        self.id = user_id
-        client = pymongo.MongoClient('localhost', 27017)
-        self.db = db = client["stalker_rp"]["users"]
-        self.json = db.find_one({"id": user_id})
-        self.is_reg = not self.json is None
-        print(self.json)
+    def __init__(self, discord_id: int):
+        self.id = discord_id
+        client = pymongo.MongoClient("localhost", 27017)
+        self.collection = client["stalker_rp"]["users"]
+        self.data = self.collection.find_one({"id": self.id})
+        self.is_reg = bool(self.data)
+
+    def get_data(self):
+        return self.collection.find_one({"id": self.id})
+
+    def update(self, key, value):
+        self.collection.update_one({"id": self.id}, {'$set': {key: value}})
+
+    def add_to_inventory(self, item):
+        self.collection.update_one({"id": self.id}, {'$push': {"inventory": item}})
+
+    def remove_from_inventory(self, item_id):
+        self.collection.update_one({"id": self.id}, {'$pull': {"inventory": {"_id": ObjectId(item_id)}}})
 
     def __getitem__(self, item):
-        return self.json[item]
-
-    def dump(self):
-        pass
+        return self.get_data()[item]
 
 
 class UserJson:

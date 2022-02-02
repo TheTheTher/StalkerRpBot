70084de97873957e41b3c6ced19acb04be4fa256
Mister-Stalker <pna24052006@yandex.ru>
2022-02-02 14:07:02 +0300

    Update dev-tool.py

--- dev-tool.py
+++ dev-tool.py
@@ -1,1 +1,19 @@
+import pymongo
 
+
+def update_item(item: dict, mask: dict):
+
+    edited_item = mask | item
+
+    for p in edited_item.keys():
+        if type(edited_item[p]) == dict:
+            edited_item[p] = mask[p] | edited_item[p]
+    return edited_item
+
+
+def update_db(db_name: str, mask: dict):
+    client = pymongo.MongoClient("localhost", 27017)
+    collection = client["stalker_rp"][db_name]
+    for item in collection.find({}):
+        collection.replace_one({"_id": item["_id"]}, update_item(item, mask), upsert=False)
+        
